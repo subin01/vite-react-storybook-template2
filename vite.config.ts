7b54d17b949ae9e4e9e50908fe426fe32bec1db4
Subin Paul <subin01@gmail.com>
2022-11-15 23:00:18 +1000

    step4: Vite multiple apps, build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    polyfillModulePreload: false,
+    // target: 'esnext',
+    // sourcemap: true,
+    minify: false,
+    cssCodeSplit: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        main2: resolve(__dirname, "app2.html"),
+        all: resolve(__dirname, "all.html"),
+        sitecore: resolve(__dirname, "sitecore.html"),
+        lib: resolve(__dirname, "/src/lib.js"),
+      },
+      output: {
+        assetFileNames: "assets/[name][extname]", // default: "assets/[name]-[hash][extname]"
+        chunkFileNames: "chunk-[name].js", // Default: "[name]-[hash].js"
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
